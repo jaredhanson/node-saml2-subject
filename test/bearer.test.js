1796fe22132a4ece356382838981594feae48839
Jared Hanson <jaredhanson@gmail.com>
2013-07-28 20:13:01 -0700

    Implement bearer confirmation method.

---
+++ test/bearer.test.js
@@ -0,0 +1,39 @@
+var subject = require('index')
+  , fs = require('fs');
+
+describe('Bearer confirmation method', function() {
+  
+  var confirmer = new subject.Confirmer();
+  confirmer.use('urn:oasis:names:tc:SAML:2.0:cm:bearer', subject.methods.bearer(function(recipient, irt, done) {
+    if (recipient == 'http://lucy.localtunnel.me/login/SAML2/POST' &&
+        irt == '_eyseyyR7kTUe3pTQIQVJkPxZcf1KBuwh') {
+      return done(null, true);
+    }
+    return done(null, false);
+  }));
+  
+  describe('confirming data with expected values', function() {
+    var xml = fs.readFileSync(__dirname + '/data/bearer-no-expiry.xml', 'utf8');
+    
+    it('should be confirmed', function(done) {
+      confirmer.confirm(xml, function(err, ok) {
+        if (err) return done(err);
+        expect(ok).to.be.true;
+        done();
+      });
+    });
+  });
+    
+  describe('confirming expired data', function() {
+    var xml = fs.readFileSync(__dirname + '/data/bearer-expired.xml', 'utf8');
+    
+    it('should not be confirmed', function(done) {
+      confirmer.confirm(xml, function(err, ok) {
+        if (err) return done(err);
+        expect(ok).to.be.false;
+        done();
+      });
+    });
+  });
+  
+});
--- test/module.test.js
+++ test/module.test.js
@@ -6,4 +6,9 @@ describe('saml2-subject', function() {
     expect(subject.Confirmer).to.be.a('function');
   });
   
+  it('should export methods', function() {
+    expect(Object.keys(subject.methods)).to.have.length(1);
+    expect(subject.methods.bearer).to.be.a('function');
+  });
+  
 });
